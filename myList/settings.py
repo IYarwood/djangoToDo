935bd92400036d9f8c00912fac0bb5fc90f86ad8
IYarwood <ianzyarwood@gmail.com>
2025-04-30 14:28:11 -0400

    changed environment variables

--- myList/settings.py
+++ myList/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.environ.get('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get().split(" ")
 
 
 # Application definition
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'myList.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -88,10 +88,9 @@ DATABASES = {
         'PORT': '5432',  # Default PostgreSQL port
     }
 }
-"""
-DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
-}
+
+databaseURL = os.environ.get("DATABASE_URL")
+DATABASES['default'] = dj_database_url.parse(databaseURL)
 """
 DATABASES = {
     'default': {
